ebc420433d1ce079eccb904a08c44c79a30bbfaf
Chadha HAJJI <chadha.hadji@gmail.com>
2022-08-01 09:20:32 +0100

    fixing something

--- src/components/Pages/Cours/CoursPage.js
+++ src/components/Pages/Cours/CoursPage.js
@@ -22,10 +22,10 @@ const CoursPage = () => {
 
   const modules = async () => {
     const response = await axios.get(
-      `http://localhost:8000/cours/detailsModule/${id}`
+      `http://localhost:8000/cours/details/${id}`
     );
     setModule(response.data);
-    console.log("response details", response.data);
+    console.log("response details of something", response.data);
   };
   const history = useNavigate();
 
@@ -41,34 +41,41 @@ const CoursPage = () => {
           direction="row"
           alignItems="center"
         >
+          {/* {module.modules.map((item) => (
+            <div>{item && item.titre_module}</div>
+          ))} */}
           <Grid item md={4}>
-            <Card sx={{ maxWidth: 345 }}>
-              <CardActionArea>
-                {/* <CardMedia
+            {module.modules?.map((item) => (
+              <Card sx={{ maxWidth: 345 }}>
+                <CardActionArea>
+                  {/* <CardMedia
                 component="img"
                 height="180"
                 image={image_cours}
                 alt="green iguana"
               /> */}
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">
-                    Titre : {module.titre_module}
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    Cours : {module.cours_module}
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-              <CardActions>
-                <Button
-                  size="small"
-                  color="primary"
-                  onClick={() => history("/cours")}
-                >
-                  Retour
-                </Button>
-              </CardActions>
-            </Card>
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      {/* Titre : {module.modules && module.modules.titre_module} */}
+                      Titre : {item && item.titre_module}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      {/* Cours : {module.modules && module.modules.cours_module} */}
+                      Cours : {item && item.cours_module}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <Button
+                    size="small"
+                    color="primary"
+                    onClick={() => history("/cours")}
+                  >
+                    Retour
+                  </Button>
+                </CardActions>
+              </Card>
+            ))}
           </Grid>
         </Grid>
       </Container>
