fd0ec536cc877c859e3ed0d33f4d47e483cb9141
chadhaaa <chadha.hadji@gmail.com>
2022-08-19 16:30:55 +0100

    Protected routes and Profile working fine and Adding dialog Form for updating profile

--- src/components/Pages/Profile/ProfileInfos.js
+++ src/components/Pages/Profile/ProfileInfos.js
@@ -4,11 +4,22 @@ import React, { useEffect, useState, useContext } from "react";
 import PrimarySearchAppBar from "../../Dashboard/Default/Navbar";
 import userimg from "../../../assets/images/user-profile/userprofile.jpg";
 import "./ProfileInfos.css";
-import { useNavigate, useParams } from "react-router-dom";
+import { useNavigate, useParams, Link } from "react-router-dom";
 import axios from "axios";
 import { saveAs } from "file-saver";
 import AuthContext from "../../../context/AuthContext";
 import jwt_decode from "jwt-decode";
+import { styled } from "@mui/material/styles";
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import DialogActions from "@mui/material/DialogActions";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
+import Typography from "@mui/material/Typography";
+import PropTypes from "prop-types";
+import { Btn, H4, H6 } from "../../../AbstractElements";
+import { Form, FormGroup, Input, Label } from "reactstrap";
 
 const PageProfil = () => {
   const [users, setUser] = useState([]);
@@ -28,6 +39,15 @@ const PageProfil = () => {
   console.log("wllh heedha", token);
 
   const history = useNavigate();
+
+  const BootstrapDialog = styled(Dialog)(({ theme }) => ({
+    "& .MuiDialogContent-root": {
+      padding: theme.spacing(2),
+    },
+    "& .MuiDialogActions-root": {
+      padding: theme.spacing(1),
+    },
+  }));
   // const id = jwt_decode("Bearer " + localStorage.getItem("jwtToken")).user_id;
   // console.log("HADHA ID", id);
   const fetchData = async () => {
@@ -78,6 +98,45 @@ const PageProfil = () => {
   useEffect(() => {
     fetchData();
   }, []);
+
+  // Dialog Update Profile
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const BootstrapDialogTitle = (props) => {
+    const { children, onClose, ...other } = props;
+
+    return (
+      <DialogTitle sx={{ m: 0, p: 2 }} {...other}>
+        {children}
+        {onClose ? (
+          <IconButton
+            aria-label="close"
+            onClick={onClose}
+            sx={{
+              position: "absolute",
+              right: 8,
+              top: 8,
+              color: (theme) => theme.palette.grey[500],
+            }}
+          >
+            <CloseIcon />
+          </IconButton>
+        ) : null}
+      </DialogTitle>
+    );
+  };
+  BootstrapDialogTitle.propTypes = {
+    children: PropTypes.node,
+    onClose: PropTypes.func.isRequired,
+  };
+
+  // Handle Update Profile
 
   return (
     <>
@@ -195,7 +254,232 @@ const PageProfil = () => {
         <br></br>
         <br></br>
 
-        <button className="btn-update">MODIFIER MES INFOS</button>
+        <button className="btn-update" onClick={handleClickOpen}>
+          MODIFIER MES INFOS
+        </button>
+        <BootstrapDialog
+          onClose={handleClose}
+          aria-labelledby="customized-dialog-title"
+          open={open}
+        >
+          <BootstrapDialogTitle
+            id="customized-dialog-title"
+            onClose={handleClose}
+          >
+            Update Your Profile
+          </BootstrapDialogTitle>
+          <DialogContent dividers>
+            {/* <Typography gutterBottom>
+              Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+              dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta
+              ac consectetur ac, vestibulum at eros.
+            </Typography>
+            <Typography gutterBottom>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur
+              et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor
+              auctor.
+            </Typography>
+            <Typography gutterBottom>
+              Aenean lacinia bibendum nulla sed consectetur. Praesent commodo
+              cursus magna, vel scelerisque nisl consectetur et. Donec sed odio
+              dui. Donec ullamcorper nulla non metus auctor fringilla.
+            </Typography> */}
+            <Form className="theme-form login-form">
+              <FormGroup>
+                <Label>Nom</Label>
+                <div className="input-group">
+                  <span className="input-group-text">
+                    <i className="icon-email"></i>
+                  </span>
+                  <Input
+                    id="nom"
+                    className="form-control"
+                    type="text"
+                    required=""
+                    placeholder="nom"
+                  />
+                </div>
+              </FormGroup>
+
+              <FormGroup className="position-relative">
+                <Label>Prénom</Label>
+                <div className="input-group">
+                  <span className="input-group-text">
+                    <i className="icon-lock"></i>
+                  </span>
+                  <Input
+                    id="prenom"
+                    className="form-control"
+                    type="text"
+                    name="prenom"
+                    required=""
+                    placeholder="Prénom"
+                  />
+                </div>
+              </FormGroup>
+              <FormGroup className="position-relative">
+                <Label>Adresse Email</Label>
+                <div className="input-group">
+                  <span className="input-group-text">
+                    <i className="icon-lock"></i>
+                  </span>
+                  <Input
+                    id="email"
+                    className="form-control"
+                    type="text"
+                    name="email"
+                    required=""
+                    placeholder="Adresse Email"
+                  />
+                </div>
+              </FormGroup>
+              <FormGroup className="position-relative">
+                <Label>Date de Naissance</Label>
+                <div className="input-group">
+                  <span className="input-group-text">
+                    <i className="icon-lock"></i>
+                  </span>
+                  <Input
+                    id="date_de_naissance"
+                    className="form-control"
+                    type="date"
+                    name="date_de_naissance"
+                    required=""
+                    placeholder="Date de Naissance"
+                  />
+                </div>
+              </FormGroup>
+              <FormGroup className="position-relative">
+                <Label>Adresse</Label>
+                <div className="input-group">
+                  <span className="input-group-text">
+                    <i className="icon-lock"></i>
+                  </span>
+                  <Input
+                    id="adresse"
+                    className="form-control"
+                    type="text"
+                    name="adresse"
+                    required=""
+                    placeholder="Adresse"
+                  />
+                </div>
+              </FormGroup>
+              <FormGroup className="position-relative">
+                <Label>Ville</Label>
+                <div className="input-group">
+                  <span className="input-group-text">
+                    <i className="icon-lock"></i>
+                  </span>
+                  <Input
+                    id="ville"
+                    className="form-control"
+                    type="text"
+                    name="ville"
+                    required=""
+                    placeholder="Ville"
+                  />
+                </div>
+              </FormGroup>
+              <FormGroup className="position-relative">
+                <Label>Code Postal</Label>
+                <div className="input-group">
+                  <span className="input-group-text">
+                    <i className="icon-lock"></i>
+                  </span>
+                  <Input
+                    id="code_postal"
+                    className="form-control"
+                    type="text"
+                    name="code_postal"
+                    required=""
+                    placeholder="Code Postal"
+                  />
+                </div>
+              </FormGroup>
+
+              <FormGroup className="position-relative">
+                <Label>Numéro de Téléphone</Label>
+                <div className="input-group">
+                  <span className="input-group-text">
+                    <i className="icon-lock"></i>
+                  </span>
+                  <Input
+                    id="num_tel"
+                    className="form-control"
+                    type="phone"
+                    name="num_tel"
+                    required=""
+                    placeholder="Numéro de Téléphone"
+                  />
+                </div>
+              </FormGroup>
+              <FormGroup className="position-relative">
+                <Label>Présentation</Label>
+                <div className="input-group">
+                  <span className="input-group-text">
+                    <i className="icon-lock"></i>
+                  </span>
+                  <Input
+                    id="presentation"
+                    className="form-control"
+                    type="text"
+                    name="presentation"
+                    required=""
+                    placeholder="Une description"
+                  />
+                </div>
+              </FormGroup>
+              <FormGroup className="position-relative">
+                <Label>Photo de Profil</Label>
+                <div className="input-group">
+                  <span className="input-group-text">
+                    <i className="icon-lock"></i>
+                  </span>
+                  <Input
+                    id="num_tel"
+                    className="form-control"
+                    type="file"
+                    name="num_tel"
+                    required=""
+                  />
+                </div>
+              </FormGroup>
+              <FormGroup className="position-relative">
+                <Label>CV</Label>
+                <div className="input-group">
+                  <span className="input-group-text">
+                    <i className="icon-lock"></i>
+                  </span>
+                  <Input
+                    id="resume"
+                    className="form-control"
+                    type="file"
+                    name="resume"
+                    required=""
+                  />
+                </div>
+              </FormGroup>
+
+              <FormGroup>
+                <Btn
+                  attrBtn={{
+                    color: "#014AAD",
+                    type: "submit",
+                  }}
+                >
+                  {/* <Link to="/dashboard" /> */}
+                  Update
+                </Btn>
+              </FormGroup>
+            </Form>
+          </DialogContent>
+          {/* <DialogActions>
+            <Button autoFocus onClick={handleClose}>
+              Save changes
+            </Button>
+          </DialogActions> */}
+        </BootstrapDialog>
       </Container>
     </>
   );
