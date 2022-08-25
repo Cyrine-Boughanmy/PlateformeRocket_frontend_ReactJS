1640e5b0d756530aaef6b4b18527b0265a298ebb
Cyrine <boughanmycyrine@gmail.com>
2022-08-25 13:52:37 +0100

    adding filter

--- src/components/Pages/Cours/AllCours.js
+++ src/components/Pages/Cours/AllCours.js
@@ -4,22 +4,29 @@ import { useEffect, useState, useContext } from "react";
 import CoursPage from "./CoursPage";
 import { Card, Grid } from "@material-ui/core";
 import {
+  Box,
   CardActionArea,
   CardContent,
   CardMedia,
   Container,
   Divider,
+  FormControl,
   IconButton,
+  InputLabel,
+  MenuItem,
   Paper,
+  Select,
   Typography,
 } from "@mui/material";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import { useNavigate } from "react-router-dom";
 import AuthContext from "../../../context/AuthContext";
 import PageLoader from "../hooks/PageLoader";
+// import Select from "react-select";
 
 const AllCours = () => {
   const [cours, setCours] = useState([]);
+  const [filteredCours, setFilteredCours] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [modules, setModules] = useState([]);
   const [category,setCategories]=useState([]);
@@ -89,14 +96,39 @@ const AllCours = () => {
   const history = useNavigate();
   
   
-  
+  // function filterPokemon(pokeType) {
+  //   let filtredPokemon = getCourses().filter(type => type.categorie === pokeType);
+  //   return filtredPokemon;
+  // }
+  // const [filtredPokemon, setFiltredPokemon] = useState(null);
+  // useEffect(() => {
+  //   setFiltredPokemon(getCourses());
+  // }, []);
+
+  // function handlePokemon(e) {
+  //   let typePokemon = e.target.value;
+  //   typePokemon !== "all"
+  //     ? setFiltredPokemon(filterPokemon(typePokemon))
+  //     : setFiltredPokemon(getCourses());
+  // }
+
+
+  const [age, setAge] = React.useState('');
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+    console.log("event.target.value",event.target.value);
+  };
   
   const filterItem = (cat) => {
     
     const updatedItems = cours.filter((curElem) => {
       return curElem.categorie === cat
     }) ;
+    console.log("updatedItems", updatedItems);
     setCours(updatedItems);
+    
+    console.log("cours", cours);
 
   }
 
@@ -110,7 +142,69 @@ const AllCours = () => {
   alignItems="flex-start"
 >
         <Grid item ><p>Filter tab</p>
-        <button onClick={()=> getCourses()}>toutes les catégories</button>
+        <br></br>
+        <Box sx={{ minWidth: 120 }}>
+          {/* <select name="catégorie :" onChange={handleChange}>
+          {category.map((cat, index) => (
+            <option key={index} value={cat.nom}>
+                 {cat.nom}
+            </option>
+          ))}
+          </select> */}
+        {/* <Select
+        value={category}
+        onChange={(item) => {
+          console.log("item",item);
+          setAge(item);
+        }}
+        options={category.map((cat, index) => {
+          return {
+            label: cat.nom,
+            value: cat.nom,
+            key: index,
+         
+          };
+          
+          
+        })}
+      /> */}
+     
+    
+     <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">categorie</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={category}
+          label="Categorie"
+          onChange={(e) => {
+            console.log("item",e);
+            handleChange(e);
+          }}
+        >
+          <MenuItem
+            onClick={() => {
+              getCourses();
+            }}
+            value="all"
+          >
+            Toutes les catégories
+          </MenuItem>
+          {category.map((categ, id) => (
+            <MenuItem
+              key={id}
+              onClick={() => {
+                filterItem(categ.nom);
+              }}
+              value={categ.nom}
+            >
+              {categ.nom}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+        </Box>
+        {/* <button onClick={()=> setCours(cours)}>toutes les catégories</button>
 
         {category.map((categ,id )=> ( 
           <button
@@ -119,15 +213,15 @@ const AllCours = () => {
             filterItem(categ.nom);
         }}
           >{categ.nom}</button>
-         ))}
-        
+         ))} */}
+       
         </Grid>
         </Grid>
 
         <Grid container spacing={3}>
         
-        {cours.map((courses) => (
-          <Grid item key={courses.id} xs={12} md={6} lg={4}>
+        {cours.map((courses,id) => (
+          <Grid item key={id} xs={12} md={6} lg={4}>
             <Card elevation={3}>
               <CardActionArea>
                 <CardContent>
